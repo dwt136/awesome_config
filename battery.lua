bc057094de28c503316d7f522bd6b99c892b4525
代文韬 <dwt136@163.com>
2014-11-01 16:42:11 +0800

    feature: change battery bar into a 1 width line when fully charged

--- battery.lua
+++ battery.lua
@@ -1,10 +1,17 @@
 local awful = require("awful")
 
+local DEFAULT_WIDTH = 8
+
 function update_bat(bar)
 	fh = assert(io.popen("acpi|cut -d% -f1|cut -d' ' -f4", "r"))
 	local remain = fh:read() + 0
 	fh:close()
 	bar:set_value(remain)
+	if remain == 100 then
+		bar:set_width(1)
+	else
+		bar:set_width(DEFAULT_WIDTH)
+	end
 	if remain > 30 then
 		bar:set_color("#666666")
 	elseif remain > 12 then
@@ -15,7 +22,6 @@ function update_bat(bar)
 end
 
 bat_widget = awful.widget.progressbar()
-bat_widget:set_width(8)
 bat_widget:set_border_color("#666666")
 bat_widget:set_background_color("#000000")
 bat_widget:set_vertical(true)
@@ -25,5 +31,4 @@ update_bat(bat_widget)
 batwidgettimer = timer({timeout = 30})
 batwidgettimer:connect_signal("timeout", function() update_bat(bat_widget) end)
 batwidgettimer:start()
-
 
